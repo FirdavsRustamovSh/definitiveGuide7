3007f4e35e9db1bf2ef9443ff3407d228ab74aeb
FirdavsRustamovSh <firdavsrustamovsh@gmail.com>
2025-04-18 18:15:40 +0500

    chapter 1

---
+++ chapter1/index.js
@@ -0,0 +1,32 @@
+// let count = 0
+// count += 2;
+// count += 2;
+// let isMatch = "two" === "two"
+
+// const plus1 = x => x + 1; 
+// const square = x => x * x; 
+// plus1(3) 
+// square(plus1(3)) 
+
+
+
+// let a = [];
+
+// a.push(1,2,3);
+// a.reverse()
+
+
+// let points=[  { x: 0, y: 0 },
+//     { x: 3, y: 4 }]
+
+// points.dist = function() { 
+//     let p1 = this[0]; 
+//     let p2 = this[1];
+//     let a = p2.x-p1.x; 
+//     let b = p2.y-p1.y; 
+//     return Math.sqrt(a*a + b*b); 
+//     };
+//     points.dist()
+    
+
+
